6e5591deb500d249fcb1366bfcfc90302ddba8e3
znb <znb.zsl@gmail.com>
2024-12-01 16:56:47 +0500

    sp work done

--- admin/specialization.php
+++ admin/specialization.php
@@ -1,5 +1,31 @@
-<?php require_once "connection.php"; ?>
+<?php
+// Establish database connection
+$con = mysqli_connect("localhost", "root", "", "hospital_management_system");
 
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Get logged-in admin details
+$admin_email = $_SESSION["admin"];
+$query = "SELECT name, email FROM reg WHERE email = ?";
+$stmt = mysqli_prepare($con, $query);
+mysqli_stmt_bind_param($stmt, "s", $admin_email);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$user = mysqli_fetch_assoc($result);
+
+$admin_name = $user['name'] ?? "Unknown";
+$admin_email = $user['email'] ?? "No email found";
+
+// Fetch specializations
+$query = "SELECT * FROM specialization";
+$sel = mysqli_query($con, $query);
+if (!$sel) {
+    die("Query failed: " . mysqli_error($con));
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,86 +34,48 @@
     <title>Admin Portal - Specializations</title>
     <link rel="stylesheet" href="css/layout.css">
     <style>
-        /* Adjustments for Main Content */
         .main-content {
             padding: 40px 20px;
             background-color: #f4f6f9;
             min-height: 100vh;
-            box-sizing: border-box;
         }
-
         .main-content h1 {
             font-size: 28px;
             color: #084d7b;
-            margin-bottom: 15px;
             text-align: center;
-        }
-
-        .main-content p {
-            font-size: 16px;
-            color: #333;
-            text-align: center;
-            margin-bottom: 25px;
         }
     </style>
     <?php require_once "mainlinks.php"; ?>
 </head>
-<body class="tt-magic-cursor">
-
-<!-- Preloader Start -->
-<div class="preloader">
-    <div class="loading-container">
-        <div class="loading"></div>
-        <div id="loading-icon"><img src="images/new_care.png" alt=""></div>
-    </div>
-</div>
-<!-- Preloader End -->
-
-<!-- Magic Cursor Start -->
-<div id="magic-cursor">
-    <div id="ball"></div>
-</div>
-<!-- Magic Cursor End -->
-
+<body>
     <div class="dashboard_container">
-        <!-- Include the header file -->
         <?php include('linkheader.php'); ?>
-
         <div class="dashboard_main">
-            <!-- Sidebar -->
             <?php include('side.php'); ?>
-
-            <!-- Main Content -->
             <div class="dashboard_content_main">
                 <div class="main-content">
                     <h1>Specializations</h1>
                     <a href="add_sp.php" class="btn btn-outline-info">Add Specialization</a>
-                <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Specialization</th>
-      <th scope="col">Edit</th>
-      <th scope="col">Delete</th>
-     </tr>
-  </thead>
-  <tbody>
-<?php
-  $query = "select * from specialization";
-  $sel = mysqli_query($con, $query);
-  while($row = mysqli_fetch_assoc($sel))
-  {
-?>
-    <tr>
-      <th scope="row"><?php echo $row["sp_id"] ?></th>
-      <th scope="row"><?php echo $row["sp_name"] ?></th>
-      <td><a href="edit_sp.php?sp_id=<?php echo $row['sp_id']; ?>" class="btn btn-outline-info">EDIT</a></td>
-      <td><a href="delete_sp.php?sp_id=<?php echo $row['sp_id']; ?>" class="btn btn-outline-warning" onclick="return confirm('Are you sure you want to delete this specialization?');">DELETE</a></td>
-    </tr>
-    <?php } ?>
-  </tbody>
-</table>
-    
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Specialization</th>
+                                <th scope="col">Edit</th>
+                                <th scope="col">Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php while ($row = mysqli_fetch_assoc($sel)): ?>
+                            <tr>
+                                <td><?php echo $row["sp_id"]; ?></td>
+                                <td><?php echo $row["sp_name"]; ?></td>
+                                <td><a href="edit_sp.php?sp_id=<?php echo $row['sp_id']; ?>" class="btn btn-outline-info">EDIT</a></td>
+                                <td><a href="delete_sp.php?sp_id=<?php echo $row['sp_id']; ?>" class="btn btn-outline-warning" onclick="return confirm('Are you sure you want to delete this specialization?');">DELETE</a></td>
+                            </tr>
+                        <?php endwhile; ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
